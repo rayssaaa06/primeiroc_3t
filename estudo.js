145b2bd1c6e9eb2a2125eb3a3805f84e0326658d
rayssa <110185261+rayssaaa06@users.noreply.github.com>
2022-10-05 09:59:45 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,18 +1,34 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Primeiro C</title>
-    <script src="estudo.js"></script>
-</head>
-<body>
-    <button onclick="escreva()">Clique aqui</button>
-    <button onclick="escreva()">Clique aqui</button>
-    <button onclick="cubo()">Calcula cubo</button>
-    
-    
-</body>
-</html>
+
+ let tabuada = 5;
+
+function escreva(){
+    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
+}
+function minhaTabuada() {
+    for(let i = 10;  i <= 1; i++) { // começa com for[
+         for (let j = 1; j<=10; j++)
+              document.write( i + " x " + j + " = " + (j*i) +"<br>");  
+         }
+
+    ]
+        document.write("i = " + i + "<BR>");
+
+   }
+}
+
+function cubo (){
+    for(let i = 20; i <= 2; i--){
+        document.write("O cubo de " + i + " x " + i + " x " +i+" = " +(i*i*i)+"br");
+    }
+
+}
 
